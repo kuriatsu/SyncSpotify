94ae53ba6e600a2ad254df698b6fc68c46bc7448
Kuribayashi Atsushi <kuriatsubayashi712@gmail.com>
2025-11-03 21:11:23 +0900

    Add format parameter to download and save functions
    Updated functions to accept a format parameter for downloading and saving songs. Adjusted download and save logic to handle different audio formats.

--- sync_spotify.py
+++ sync_spotify.py
@@ -11,32 +11,37 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
                                     client_id=os.environ.get("CLIENT_ID"),
                                     client_secret=os.environ.get("CLIENT_SECRET"),
                                     redirect_uri="http://localhost:8888/callback",
-                                    scope="user-library-read"
+                                    scope="user-library-read",
+                                   
                                     )
                     )
 
-def download(url):
+def download(url, format):
     # command = ["spotify-ripper", "--flac", "-Q", "320", "-f", "{track_name}.{ext}", "-d", dir, "-l", uri]
     ## when scan and skip
     # command = ["spotdl", "download", url, "--overwrite skip --scan-for-songs"]
-    command = ["spotdl", "download", url]
+    if format == "flac":
+        command = ["spotdl", "download", url, "--format flac"]
+    else:
+        command = ["spotdl", "download", url]
+
     subprocess.call(command)
 
-def save_song(item):
+def save_song(item, format):
     print("====================")
 
     ## get file name
     output_mp3_name = f"{item['track']['artists'][0]['name']}"
     if len(item['track']['artists']) == 1:
-        output_mp3_name += f" - {item['track']['name']}.mp3" 
+        output_mp3_name += f" - {item['track']['name']}.{format}" 
     else:
         for artist in item['track']['artists'][1:]:
             output_mp3_name += ", " + artist["name"]
-        output_mp3_name += f" - {item['track']['name']}.mp3"
+        output_mp3_name += f" - {item['track']['name']}.{format}"
 
 
     if not os.path.isfile(output_mp3_name):
-        output_mp3_name = f"{item['track']['artists'][0]['name']} - {item['track']['name']}.mp3"
+        output_mp3_name = f"{item['track']['artists'][0]['name']} - {item['track']['name']}.{format}"
 
     print(f"{output_mp3_name}")
     is_update = True
@@ -62,23 +67,24 @@ def save_song(item):
 
     if is_update:
         try:
-            download(item["track"]["external_urls"]["spotify"])
+            download(item["track"]["external_urls"]["spotify"], format)
         except Exception as e:
             print(e)
 
-def sync(tracks):
+def sync(tracks, format):
 
     download_list = "" 
     for item in tracks['items']:
-        save_song(item)
+        save_song(item, "mp3")
 
     while tracks['next']:
         tracks = sp.next(tracks)
         for item in tracks['items']:
-            save_song(item)
+            save_song(item, format)
 
 def main():
 
+    format = "mp3"
     playlist = "liked" if len(sys.argv) == 1 else sys.argv[1]
 
     if playlist == "liked":
@@ -86,7 +92,7 @@ def main():
     else:
         tracks = sp.playlist(playlist)["tracks"]
 
-    sync(tracks)
+    sync(tracks, format)
 
 if __name__ == "__main__":
     main()
